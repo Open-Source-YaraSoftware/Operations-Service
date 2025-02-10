24993b539ce152413153f693fae59d7897c7c09f
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-02-10 18:00:53 -0500

    feat(work-order): add enums for priority, status and request type

---
+++ src/main/java/com/workshopngine/platform/serviceoperations/operations/domain/model/valueobjects/EWorkOrderStatus.java
@@ -0,0 +1,10 @@
+package com.workshopngine.platform.serviceoperations.operations.domain.model.valueobjects;
+
+public enum EWorkOrderStatus {
+    CREATED,
+    ASSIGNED,
+    IN_PROGRESS,
+    PAUSED,
+    COMPLETED,
+    CANCELLED
+}
