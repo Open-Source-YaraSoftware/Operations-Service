1e60551a6be824525309ace5e5209b22bd0a1ac8
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-02-14 20:16:18 -0500

    refactor: appointmentId could be null

--- src/main/java/com/workshopngine/platform/serviceoperations/operations/domain/model/valueobjects/AppointmentId.java
+++ src/main/java/com/workshopngine/platform/serviceoperations/operations/domain/model/valueobjects/AppointmentId.java
@@ -4,9 +4,4 @@ import jakarta.persistence.Embeddable;
 
 @Embeddable
 public record AppointmentId(String appointmentId) {
-    public AppointmentId {
-        if (appointmentId == null || appointmentId.isBlank()) {
-            throw new IllegalArgumentException("AppointmentId cannot be null or empty");
-        }
-    }
 }
