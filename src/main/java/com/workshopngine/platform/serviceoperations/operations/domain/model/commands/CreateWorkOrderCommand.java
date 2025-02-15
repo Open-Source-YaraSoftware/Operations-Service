1495ef83b2b6faf75bf2fafb12cc482b1426c6d2
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-02-14 20:17:10 -0500

    feat(domain): add CreateWorkOrderCommand and GetWorkOrderByIdQuery

---
+++ src/main/java/com/workshopngine/platform/serviceoperations/operations/domain/model/commands/CreateWorkOrderCommand.java
@@ -0,0 +1,14 @@
+package com.workshopngine.platform.serviceoperations.operations.domain.model.commands;
+
+import com.workshopngine.platform.serviceoperations.operations.domain.model.valueobjects.*;
+
+public record CreateWorkOrderCommand(
+        ClientId clientId,
+        VehicleId vehicleId,
+        AppointmentId appointmentId,
+        WorkshopId workshopId,
+        MechanicId mechanicId,
+        EPriority priority,
+        ERequestType requestType
+) {
+}
