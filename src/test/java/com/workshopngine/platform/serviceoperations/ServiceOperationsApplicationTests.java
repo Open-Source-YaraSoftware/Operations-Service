6c62c1d4ce8c22aaa4dbc3d11f45726567015a79
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-02-17 01:34:34 -0500

    test(application): add Testcontainers support for MySQL in integration tests

--- src/test/java/com/workshopngine/platform/serviceoperations/ServiceOperationsApplicationTests.java
+++ src/test/java/com/workshopngine/platform/serviceoperations/ServiceOperationsApplicationTests.java
@@ -1,10 +1,20 @@
 package com.workshopngine.platform.serviceoperations;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @SpringBootTest
 class ServiceOperationsApplicationTests {
+	@Container
+	@ServiceConnection
+	static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.0.26");
 
 	@Test
 	void contextLoads() {
