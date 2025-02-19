aeb693e7ee09188ca596c3638db1638a4925ef4d
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-02-19 18:01:21 -0500

    feat(services): define `DiagnosticCommandService` and `DiagnosticQueryService`

---
+++ src/main/java/com/workshopngine/platform/serviceoperations/operations/domain/services/DiagnosticQueryService.java
@@ -0,0 +1,20 @@
+package com.workshopngine.platform.serviceoperations.operations.domain.services;
+
+import com.workshopngine.platform.serviceoperations.operations.domain.model.aggregates.Diagnostic;
+import com.workshopngine.platform.serviceoperations.operations.domain.model.entities.DiagnosticFinding;
+import com.workshopngine.platform.serviceoperations.operations.domain.model.entities.Evidence;
+import com.workshopngine.platform.serviceoperations.operations.domain.model.entities.Recommendation;
+import com.workshopngine.platform.serviceoperations.operations.domain.model.queries.*;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface DiagnosticQueryService {
+    Optional<Diagnostic> handle(GetDiagnosticByIdQuery query);
+    Collection<Diagnostic> handle(GetAllDiagnosticsByWorkshopIdQuery query);
+    Collection<Diagnostic> handle(GetAllDiagnosticsByVehicleIdQuery query);
+    Optional<DiagnosticFinding> handle(GetDiagnosticFindingByIdQuery query);
+    Collection<DiagnosticFinding> handle(GetAllDiagnosticFindingsByDiagnosticIdQuery query);
+    Collection<Evidence> handle(GetAllEvidencesByDiagnosticFindingIdQuery query);
+    Collection<Recommendation> handle(GetAllRecommendationByDiagnosticFindingIdQuery query);
+}
