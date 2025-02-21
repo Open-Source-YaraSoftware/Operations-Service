cecda3814b0b6e966e3b3c63e13ab3e9c75a6d63
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-02-20 22:25:26 -0500

    refactor(dto): add @Builder annotation to CreateDiagnosticFindingResource and CreateDiagnosticResource

--- src/main/java/com/workshopngine/platform/serviceoperations/operations/interfaces/rest/dto/CreateDiagnosticFindingResource.java
+++ src/main/java/com/workshopngine/platform/serviceoperations/operations/interfaces/rest/dto/CreateDiagnosticFindingResource.java
@@ -1,7 +1,10 @@
 package com.workshopngine.platform.serviceoperations.operations.interfaces.rest.dto;
 
+import lombok.Builder;
+
 import java.math.BigDecimal;
 
+@Builder
 public record CreateDiagnosticFindingResource(
         String description,
         String severity,
