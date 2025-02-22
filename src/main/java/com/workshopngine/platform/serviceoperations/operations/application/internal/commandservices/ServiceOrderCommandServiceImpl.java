9b584b0f5c2a43df9e9260e3da422e9b47906f34
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-02-21 20:18:34 -0500

    feat(service-order): implement service order command and query services
    - Updated `ServiceOrderCommandServiceImpl` to handle `CreateServiceOrderCommand` and persist `ServiceOrder`.
    - Injected `ServiceOrderRepository` into `ServiceOrderCommandServiceImpl` for data persistence.
    - Updated `ServiceOrderQueryServiceImpl` to retrieve `ServiceOrder` by ID from the repository.
    - Refactored code to use `@RequiredArgsConstructor` for dependency injection.

--- src/main/java/com/workshopngine/platform/serviceoperations/operations/application/internal/commandservices/ServiceOrderCommandServiceImpl.java
+++ src/main/java/com/workshopngine/platform/serviceoperations/operations/application/internal/commandservices/ServiceOrderCommandServiceImpl.java
@@ -1,13 +1,25 @@
 package com.workshopngine.platform.serviceoperations.operations.application.internal.commandservices;
 
-import com.workshopngine.platform.serviceoperations.operations.domain.model.commands.CreateWorkOrderCommand;
+import com.workshopngine.platform.serviceoperations.operations.domain.model.aggregates.ServiceOrder;
+import com.workshopngine.platform.serviceoperations.operations.domain.model.commands.CreateServiceOrderCommand;
 import com.workshopngine.platform.serviceoperations.operations.domain.services.ServiceOrderCommandService;
+import com.workshopngine.platform.serviceoperations.operations.infrastructure.persistence.jpa.repositories.ServiceOrderRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class ServiceOrderCommandServiceImpl implements ServiceOrderCommandService {
+    private final ServiceOrderRepository serviceOrderRepository;
+
     @Override
-    public String handle(CreateWorkOrderCommand command) {
-        return "";
+    public String handle(CreateServiceOrderCommand command) {
+        ServiceOrder workOrder = new ServiceOrder(command);
+        try {
+            serviceOrderRepository.save(workOrder);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Error while saving work order: " + e.getMessage());
+        }
+        return workOrder.getId();
     }
 }
--- src/main/java/com/workshopngine/platform/serviceoperations/operations/application/internal/queryservices/ServiceOrderQueryServiceImpl.java
+++ src/main/java/com/workshopngine/platform/serviceoperations/operations/application/internal/queryservices/ServiceOrderQueryServiceImpl.java
@@ -3,14 +3,19 @@ package com.workshopngine.platform.serviceoperations.operations.application.inte
 import com.workshopngine.platform.serviceoperations.operations.domain.model.aggregates.ServiceOrder;
 import com.workshopngine.platform.serviceoperations.operations.domain.model.queries.GetServiceOrderByIdQuery;
 import com.workshopngine.platform.serviceoperations.operations.domain.services.ServiceOrderQueryService;
+import com.workshopngine.platform.serviceoperations.operations.infrastructure.persistence.jpa.repositories.ServiceOrderRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@RequiredArgsConstructor
 @Service
 public class ServiceOrderQueryServiceImpl implements ServiceOrderQueryService {
+    private final ServiceOrderRepository serviceOrderRepository;
+
     @Override
     public Optional<ServiceOrder> handle(GetServiceOrderByIdQuery query) {
-        return Optional.empty();
+        return serviceOrderRepository.findById(query.serviceOrderId());
     }
 }
