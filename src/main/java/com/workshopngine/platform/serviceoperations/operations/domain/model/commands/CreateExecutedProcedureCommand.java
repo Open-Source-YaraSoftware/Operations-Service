07158a52eec42858b31600662c5169ec49e3b308
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-02-22 02:12:07 -0500

    refactor(executed-procedure): enhance CreateExecutedProcedureCommand with detailed attributes

--- src/main/java/com/workshopngine/platform/serviceoperations/operations/domain/model/commands/CreateExecutedProcedureCommand.java
+++ src/main/java/com/workshopngine/platform/serviceoperations/operations/domain/model/commands/CreateExecutedProcedureCommand.java
@@ -1,7 +1,16 @@
 package com.workshopngine.platform.serviceoperations.operations.domain.model.commands;
 
+import com.workshopngine.platform.serviceoperations.operations.domain.model.valueobjects.StandardProcedureId;
 import lombok.Builder;
 
+import java.time.Duration;
+
 @Builder
-public record CreateExecutedProcedureCommand() {
+public record CreateExecutedProcedureCommand(
+        String serviceOrderId,
+        StandardProcedureId standardProcedureId,
+        String name,
+        String description,
+        Duration estimatedTime
+) {
 }
