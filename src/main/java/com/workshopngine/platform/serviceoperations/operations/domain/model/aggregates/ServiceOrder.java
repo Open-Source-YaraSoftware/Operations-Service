ace7b91668e4dee14c6f8c3e2f501a11b0f476fb
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-02-21 20:16:41 -0500

    feat(service-order): implement ServiceOrder aggregate
    - Defined `ServiceOrder` as an entity extending `AuditableAbstractAggregateRoot`.
    - Added necessary attributes: `serviceType`, `workshopId`, `vehicleId`, `clientId`, `assignedMechanicId`, and `workOrderId`.
    - Introduced `EServiceOrderStatus` with default status `PENDING`.
    - Initialized `totalTimeSpent` to `Duration.ZERO`.
    - Created constructor to initialize `ServiceOrder` from `CreateServiceOrderCommand`.

--- src/main/java/com/workshopngine/platform/serviceoperations/operations/domain/model/aggregates/ServiceOrder.java
+++ src/main/java/com/workshopngine/platform/serviceoperations/operations/domain/model/aggregates/ServiceOrder.java
@@ -1,4 +1,64 @@
 package com.workshopngine.platform.serviceoperations.operations.domain.model.aggregates;
 
-public class ServiceOrder {
+import com.workshopngine.platform.serviceoperations.operations.domain.model.commands.CreateServiceOrderCommand;
+import com.workshopngine.platform.serviceoperations.operations.domain.model.valueobjects.*;
+import com.workshopngine.platform.serviceoperations.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Duration;
+
+@Entity
+@Getter
+@Setter
+public class ServiceOrder extends AuditableAbstractAggregateRoot<ServiceOrder> {
+    @NotNull
+    private EServiceType serviceType;
+
+    @NotNull
+    @Embedded
+    private WorkshopId workshopId;
+
+    @NotNull
+    @Embedded
+    private VehicleId vehicleId;
+
+    @NotNull
+    @Embedded
+    private ClientId clientId;
+
+    @NotNull
+    @Embedded
+    private MechanicId assignedMechanicId;
+
+    @NotNull
+    @Embedded
+    private WorkOrderId workOrderId;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private EServiceOrderStatus status;
+
+    private Duration totalTimeSpent;
+
+    public ServiceOrder() {
+        super();
+        this.status = EServiceOrderStatus.PENDING;
+        this.totalTimeSpent = Duration.ZERO;
+    }
+
+    public ServiceOrder(CreateServiceOrderCommand command) {
+        this();
+        this.serviceType = command.serviceType();
+        this.workshopId = command.workshopId();
+        this.vehicleId = command.vehicleId();
+        this.clientId = command.clientId();
+        this.assignedMechanicId = command.assignedMechanicId();
+        this.workOrderId = command.workOrderId();
+    }
 }
